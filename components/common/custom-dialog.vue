e17d167344bc384a0e953b03f5e790656b9c04f6
lc <846720961@qq.com>
2021-03-30 16:05:20 +0800

    feat:echart

--- components/common/custom-dialog.vue
+++ components/common/custom-dialog.vue
@@ -1,128 +1,147 @@
 <template>
-	<div>
+  <div>
     <van-overlay :show="show" @click="show = false">
-			<div ref="pop" :class="{prompt:true,big:type!='small'}">
+      <div ref="pop" :class="{ prompt: true, big: type != 'small' }">
         <van-icon name="cross" size="30" @click="closeDialog" />
-				<div v-if='subTitle' class="sub">{{subTitle}}</div>
-				<div v-if='title' class="title">
-					{{title}}
-				</div>
-				<slot></slot>
-				<div class="qrcode" v-if="type=='code'">
-					<img :src="qrcode" class="qr-img" />
-					<div class="sub-text">
-						长按二维码保存
-					</div>
-					<img @click="toLink('')" class="service-banner" src="../../assets/imgs/zeor_icon.png" />
-				</div>
-				<button v-if="btnText" class="btn" @click="confirm" >
-					{{btnText}}
-				</button>
-			</div>
-      </van-overlay>
-	</div>
+        <div v-if="subTitle" class="sub">{{ subTitle }}</div>
+        <div v-if="title" class="title">
+          {{ title }}
+        </div>
+        <slot></slot>
+        <div class="qrcode" v-if="type == 'code'">
+          <img
+            :src="qrcode"
+            id="captureId"
+            class="qr-img"
+            @touchstart="start(qrcode)"
+            @touchend="end"
+          />
+          <div class="sub-text">
+            长按二维码保存
+          </div>
+          <img
+            @click="toLink('')"
+            class="service-banner"
+            src="../../assets/imgs/zeor_icon.png"
+          />
+        </div>
+        <button v-if="btnText" class="btn" @click="confirm">
+          {{ btnText }}
+        </button>
+      </div>
+    </van-overlay>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-         show: false
-			};
-		},
-		props: {
-			subTitle: String,
-			title: String,
-			btnText:String,
-			qrcode:String,
-			defaultBtn:{
-				type:Boolean,
-				default:false
-			},
-			type:{
-				type:String,
-				default:'small'
-				},
-
-		},
-		methods: {
-			openDialog() {
-				this.show=true
-			},
-			closeDialog() {
-			this.show=false
-			},
-			confirm() {
-				if(!this.defaultBtn) this.$emit('confirm')
-				this.closeDialog()
-			},
-			toLink(){
-			/* 	uni.navigateTo({
+export default {
+  data() {
+    return {
+      show: false,
+      loop:null
+    };
+  },
+  props: {
+    subTitle: String,
+    title: String,
+    btnText: String,
+    qrcode: String,
+    defaultBtn: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: "small"
+    },
+  },
+  methods: {
+    openDialog() {
+      this.show = true;
+    },
+    closeDialog() {
+      this.show = false;
+    },
+    confirm() {
+      if (!this.defaultBtn) this.$emit("confirm");
+      this.closeDialog();
+    },
+    start(img) {
+      clearTimeout(this.loop); //再次清空定时器，防止重复注册定时器
+      this.loop = setTimeout(() => {
+        this.$utils.downloadFile(img)
+      }, 1000);
+    },
+    end() {
+      clearTimeout(this.loop); //清空定时器，防止重复注册定时器
+    },
+    toLink() {
+      /* 	uni.navigateTo({
 					url:'/pages/activity/rulesDetails'
 				}) */
-			}
-		},
-	}
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.van-overlay{
+.van-overlay {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-	.prompt {
-		background: #ffffff;
-		border-radius: 20px;
-		min-height: 305px;
-		width: 480px;
-		padding: 30px 35px;
-		&.big{
-			width: 550px;
-		}
-		.sub {
-			font-size: 26px;
-			color: #929292;
-		}
+.prompt {
+  background: #ffffff;
+  border-radius: 20px;
+  min-height: 305px;
+  width: 480px;
+  padding: 30px 35px;
+  &.big {
+    width: 550px;
+  }
+  .sub {
+    font-size: 26px;
+    color: #929292;
+  }
 
-		.title {
-			text-align: center;
-			font-size: 30px;
-			color: #333333;
-			margin: 35px 0 ;
-			font-weight: 700;
-		}
+  .title {
+    text-align: center;
+    font-size: 30px;
+    color: #333333;
+    margin: 35px 0;
+    font-weight: 700;
+  }
 
-		.btn {
-			width: 80%;
-			height: 78px;
-			background: #3BA6F4;
-			border-radius: 39px;
-			font-size: 36px;
-			color: #FFFFFF;
-			margin-top: 60px;
-		}
-		.close{
-			position: absolute;
-			right: 15px;
-			top: 15px;
-		}
-		.qrcode{
-			text-align: center;
-			.qr-img{
-				width: 280px;
-				height: 280px;
-				margin: 0 auto;
-			}
-			.sub-text{
-				font-size: 24px;
-				line-height: 24px;
-				margin:30px 0 58px 0 ;
-			}
-			.service-banner{
-				width: 480px;
-				height: 80px;
-			}
-		}
-	}
+  .btn {
+    width: 80%;
+    height: 78px;
+    background: #3ba6f4;
+    border-radius: 39px;
+    font-size: 36px;
+    color: #ffffff;
+    margin-top: 60px;
+  }
+  .close {
+    position: absolute;
+    right: 15px;
+    top: 15px;
+  }
+  .qrcode {
+    text-align: center;
+    .qr-img {
+      width: 280px;
+      height: 280px;
+      margin: 0 auto;
+    }
+    .sub-text {
+      font-size: 24px;
+      line-height: 24px;
+      margin: 30px 0 58px 0;
+    }
+    .service-banner {
+      width: 480px;
+      height: 80px;
+    }
+  }
+}
 </style>
--- plugins/vue-global.js
+++ plugins/vue-global.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Vant from 'vant';
-import echarts from 'echarts' // 引入echarts
+import * as echarts from 'echarts'
 import 'vant/lib/index.css'
 import myComponentsInstall from '~/components/myComponentsInstall'
 import utils from '~/utils/helpers'
-Vue.prototype.$echarts = echarts // 引入组件（将echarts注册为全局）
+Vue.prototype.$echarts = echarts
 Vue.use(Vant)
 Vue.use(myComponentsInstall)
 export default function (context, inject) {
